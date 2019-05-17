d80cca1702f390104236eea8fdcd2d335cf3c7f9
Austin <austinz@mac.com>
2019-05-16 19:16:11 -0700

    fixing tests for sec1

--- test/modpoint.js
+++ test/modpoint.js
@@ -5,16 +5,16 @@ try {
     const ModPoint = elliptic.ModPoint  
 
     let g = new ModPoint(
-        bigInt('95344521472007825523052856318493543457918225329593594827198878438444869079805'),
-        bigInt('85802951166965653834180459665017008093029170635302396883361952917601245373882')
+        bigInt('79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798', 16),
+        bigInt('483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8', 16)
     )
-    if (g.toString() !== '{"x":"d2cb1636c8800502112f346f10a62e256d42b5ea46b3a55e2ff4607167afd2fd","y":"bdb2bfd6280aca239796dc4eb6283ad2d31a5ef417620efb095887c7dc56a5ba"}') {
+    if (g.toString() !== '{"x":"79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798","y":"483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8"}') {
         throw 'mod point toString() failed'
     }
-    if (g.sec1Compressed !== '02d2cb1636c8800502112f346f10a62e256d42b5ea46b3a55e2ff4607167afd2fd') {
+    if (g.sec1Compressed !== '0279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798') {
         throw 'invalid sec1 (compressed)'
     }
-    if (g.sec1Uncompressed !== '04d2cb1636c8800502112f346f10a62e256d42b5ea46b3a55e2ff4607167afd2fdbdb2bfd6280aca239796dc4eb6283ad2d31a5ef417620efb095887c7dc56a5ba') {
+    if (g.sec1Uncompressed !== '0479be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8') {
         throw 'invalid sec1 (uncompressed)'
     }
 
@@ -30,15 +30,13 @@ try {
         throw `can't recover point from json`
     }
 
-    // const sec1Compressed = g_3.sec1Compressed
+    const sec1Compressed = g_3.sec1Compressed
 
-    // const g_4 = ModPoint.fromSec1(sec1Compressed, secp256k1)
+    const g_4 = ModPoint.fromSec1(sec1Compressed, secp256k1)
 
-    // console.log(g_4.toString())
-    // console.log(g_3.toString())
-    // if (g_4.toString() !== g_3.toString()) {
-    //     throw `can't recover point from sec1 (compressed) format`
-    // }
+    if (g_4.toString() !== g_3.toString()) {
+        throw `can't recover point from sec1 (compressed) format`
+    }
 
     const sec1Uncompressed = g.sec1Uncompressed
 
